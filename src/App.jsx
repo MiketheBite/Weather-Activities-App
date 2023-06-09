bb3fc7e01d7cddd4736521087a7fb467a4502cb8
Charlotte Ketteler <ketteler.cha@gmail.com>
2023-06-09 12:24:08 +0200

    AddingList

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './App.css';
 import Form from './components/Form';
 import { uid } from "uid";
 import useLocalStorageState from "use-local-storage-state";
+import List from './components/List';
 
 function App() {
   const [activities, setActivities] = useLocalStorageState("activities", { defaultValue: [] });
@@ -17,6 +18,7 @@ function App() {
 
   return (
 <Form  onAddActivity={handleAddActivity} />
+<List />
   )
 }
 
---
+++ src/components/List.jsx
@@ -0,0 +1,14 @@
+
+export default function List({activities}){
+    return <>
+    <ul>
+        {activities.map((activity) => {
+            return (
+                <li>
+
+                </li>
+            )
+        })}
+    </ul>
+    </>
+}
